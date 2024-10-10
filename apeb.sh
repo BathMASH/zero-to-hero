391522b1276d625367d61a6ac592d719385ecdc5
Ed Southwood <edsouthwood@gmail.com>
2024-10-10 10:39:35 +0100

    added apeb script

--- apeb.sh
+++ apeb.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #
-# This script copies the global policy folder and updates it to git.
+# This script copies the apeb folder and updates it to git.
 #
-echo rendering global policy version
-quarto render --profile global-policy
-echo copying to stat-tastic-global
-cp -r -u ./_global-policy/* ../stat-tastic-global
-cd ../stat-tastic-global
+echo rendering apeb version
+quarto render --profile apeb
+echo copying to apeb
+cp -r -u ./_apeb/* ../apeb
+cd ../apeb
 git add .
 git commit -m 'updated website'
 echo pushing to github
 git push origin master
-cd ../stat-tastic
+cd ../base
 echo done
